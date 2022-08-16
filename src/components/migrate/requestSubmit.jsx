0cbce7de9aef531b1a5d161ddeb3df68557fb1b2
framnz85 <clavmall.85@gmail.com>
2022-08-16 16:39:58 +0800

    make it better

--- src/components/migrate/requestSubmit.jsx
+++ src/components/migrate/requestSubmit.jsx
@@ -35,6 +35,8 @@ class MigrateSubmitted extends Component {
             <b style={{ color: "red" }}>Subject:</b> Migration Request # {this.state.estoreid}
             <br />
             <b style={{ color: "red" }}>Content:</b> Sir start migrating my account from eStore to Clavstore with Migration Request # {this.state.estoreid}
+            <br /><br /><br />
+            <div style={{border: "1px solid red", color: "red", padding: 20}}>NOTE: Please note that the Clavstore Platform is already a paid platform unlike the eStore. This is because we need to pay the hosting fee for your website. There is no final pricing for the platform yet but it would probably range from $14 to $29 per month. First payment will take efffect 7 days after your website has been activated. If this is ok with you then proceed on your next step above.</div>
           </div>
 
         </div>
